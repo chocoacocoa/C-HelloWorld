0932ef6a986228e701c2d5a0d8a1f840a73c245f
chocoacocoa <vancholo2006@gmail.com>
2026-09-15 21:14:29 +0800

    Hey sir Ive added a String typing looper, check it out.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace MyConsoleApp
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hi World");
-            Console.WriteLine("I love you");
+            Console.WriteLine("I love");
+
+            Console.WriteLine("say your text: ");
+            string UserText = Console.ReadLine();
+
+            Console.WriteLine("How many time we should iterate your text?: ");
+            int iterations = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i <= iterations; i++)
+            {
+                Console.WriteLine(UserText);
+            }
         }
     }
 }
