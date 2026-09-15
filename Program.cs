779516d2107a0084b8c17dfb78075943297b190d
chocoacocoa <vancholo2006@gmail.com>
2026-09-15 20:57:22 +0800

    first commit for masterBates branch

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MyConsoleApp
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hi World");
+            Console.WriteLine("tangina mo");
         }
     }
 }
